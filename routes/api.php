860b9c1919c2ff92778975e9d4761dc8fcf8da40
joaogabrielmndz <jgquadros2005@gmail.com>
2026-09-20 15:21:14 -0300

    feat(Routes): add endpoint POST /pages

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,13 @@
 <?php
 
+use App\Http\Controllers\PageController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+Route::prefix('v1/')->middleware('auth:sanctum')->group(function () {
+    Route::post('/pages', [PageController::class, 'store']);
+});
