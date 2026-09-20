66e0e306ea791d1a4d288e66247b170b8a5ce888
joaogabrielmndz <jgquadros2005@gmail.com>
2026-09-20 15:16:33 -0300

    feat(Seeder): add in Databaseeder a creation of test user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -17,9 +18,23 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+
+        $user = User::firstOrCreate(
+            ['email' => 'test@example.com'],
+            [
+                'name' => 'Test User',
+                'password' => Hash::make('passowrd')
+            ]
+        );
+
+        $user->tokens()->delete();
+
+        $token = $user->createToken('chrome-extension-token')->plainTextToken;
+
+        $this->command->info("Token gerado: {$token}");
     }
 }
